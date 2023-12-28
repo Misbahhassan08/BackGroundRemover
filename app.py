4b5f9e62a4c86e79adadf83369429bae2c5174f2
misbahhassan08 <misbahhassan08@gmail.com>
2023-12-28 13:07:40 +0500

    add logs

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, send_file, jsonify
+from flask import Flask, render_template, request, Response, jsonify, send_file
+from pygtail import Pygtail
+import logging, os, sys, time
 from flask_socketio import SocketIO,emit
 from flask_cors import CORS, cross_origin
 import os
@@ -6,26 +8,37 @@ from PIL import Image
 import io
 import base64
 from config import *
+import logging
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '98kjhkjhkj'
-
-
+app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+CORS(app)
+app.app_context().push()
 socketio = SocketIO(app, cross_origin='*')
 
 image = None
 
+LOG_FILE = 'app.log'
+log = logging.getLogger('__name__')
+logging.basicConfig(filename=LOG_FILE, level=logging.DEBUG)
+
+def logUpdater(mesg):
+        logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+        logging.info('{}'.format(mesg))
+        pass
 
 @socketio.on('connect')
 def on_connect():
     global image
     image = None 
     print('Server received connection')
+    logUpdater("CLIENT CONNECTED")
     socketio.send("client connect")
 
 @socketio.on('pong')  
 def on_pong(msg):
     print("received pong"+msg) 
+    logUpdater("pong, recived pong from local ai server")
     socketio.emit('ping',"ping from server ")
     pass 
 
@@ -40,6 +53,7 @@ def message(data):
     global image
     image = data
     print("data received from client")  # show msg from client
+    logUpdater("data received from client")
     #emit('response', {'from': 'server'})
     
     pass
@@ -48,6 +62,13 @@ def message(data):
 def on_get_default():
     return jsonify({"Status":"OK"})
 
+@app.route('/log')
+def progress_log():
+	def generate():
+		for line in Pygtail(LOG_FILE, every_n=0):
+			yield "->" + str(line) + "\n\n"
+			
+	return Response(generate(), mimetype= 'text/event-stream')
 
 @app.route("/remove", methods=["POST"])
 def remove():
